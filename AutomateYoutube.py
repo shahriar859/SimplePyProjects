b9fd28014a2513c45dbf311c7293e9401212a198
Shahriar Mamun <71685588+shahriar859@users.noreply.github.com>
2021-02-11 16:17:36 +0600

    Create AutomateYoutube.py

---
+++ AutomateYoutube.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+
+
+class Music():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def play(self):
+        name = input("enter a youtube channel name: ")
+        self.driver.get("https://www.youtube.com/c/"+name+"/videos")
+
+        new = self.driver.find_element_by_xpath('//*[@id="items"]/ytd-grid-video-renderer[1]')
+        new.click()
+
+
+bot = Music()
+bot.play()
